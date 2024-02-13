5b89e28ef6a357b4e880c87eae7efe8a9a76a461
Hanna <hanna@trdst.com>
2024-02-13 21:44:41 +0900

    fix: ClaimController ApiResponse import 수정

--- server/api/src/main/kotlin/com/kw/api/domain/claim/controller/ClaimController.kt
+++ server/api/src/main/kotlin/com/kw/api/domain/claim/controller/ClaimController.kt
@@ -1,6 +1,6 @@
 package com.kw.api.domain.claim.controller
 
-import com.kw.api.common.dto.ApiResponse
+import com.kw.api.common.dto.response.ApiResponse
 import com.kw.api.domain.claim.dto.request.CreateClaimRequest
 import com.kw.api.domain.claim.dto.response.ClaimResponse
 import com.kw.api.domain.claim.service.ClaimService
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1")
 class ClaimController(val claimService: ClaimService) {
     @PostMapping("/claims")
-    fun createClaim(@RequestBody createClaimRequest : CreateClaimRequest) : ApiResponse<ClaimResponse> {
+    fun createClaim(@RequestBody createClaimRequest: CreateClaimRequest): ApiResponse<ClaimResponse> {
         val response = claimService.createClaim(createClaimRequest)
         return ApiResponse.created(response)
     }
