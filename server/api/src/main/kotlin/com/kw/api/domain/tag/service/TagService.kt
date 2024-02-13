a50370173a5ac5087aa2ea311306953167ff6034
KarmaPol <kkhkr98@gmail.com>
2024-02-13 10:01:32 +0900

    feat: 태그 조회 추가

---
+++ server/api/src/main/kotlin/com/kw/api/domain/tag/service/TagService.kt
@@ -0,0 +1,17 @@
+package com.kw.api.domain.tag.service
+
+import com.kw.api.domain.tag.dto.response.TagResponse
+import com.kw.data.domain.tag.repository.TagRepository
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+
+@Service
+@Transactional
+class TagService(val tagRepository: TagRepository) {
+    fun getTags() : List<TagResponse> {
+        val tags = tagRepository.findAll()
+        return tags.map { tag ->
+            TagResponse.of(tag)
+        }
+    }
+}
