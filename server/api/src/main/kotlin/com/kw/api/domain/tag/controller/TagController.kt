5fb5cb3ebd18b01269120a00aea2ba014098cee5
Hanna Lee <8annahxxl@gmail.com>
2024-02-22 11:44:16 +0900

    docs: swagger 설정

--- server/api/src/main/kotlin/com/kw/api/domain/tag/controller/TagController.kt
+++ server/api/src/main/kotlin/com/kw/api/domain/tag/controller/TagController.kt
@@ -3,13 +3,18 @@ package com.kw.api.domain.tag.controller
 import com.kw.api.common.dto.response.ApiResponse
 import com.kw.api.domain.tag.dto.response.TagResponse
 import com.kw.api.domain.tag.service.TagService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "태그")
 @RestController
 @RequestMapping("/api/v1")
 class TagController(val tagService: TagService) {
+
+    @Operation(summary = "태그 목록 조회")
     @GetMapping("/tags")
     fun getTags(): ApiResponse<List<TagResponse>> {
         val responses = tagService.getTags()
