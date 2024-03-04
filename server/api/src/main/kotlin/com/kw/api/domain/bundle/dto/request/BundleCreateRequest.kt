f24a570c3ae19b43d202ef0b7b316aae7d736df8
Hanna Lee <8annahxxl@gmail.com>
2024-03-04 10:27:35 +0900

    fix: createBundle 태그 개수 검사 서비스 로직으로 이동

--- server/api/src/main/kotlin/com/kw/api/domain/bundle/dto/request/BundleCreateRequest.kt
+++ server/api/src/main/kotlin/com/kw/api/domain/bundle/dto/request/BundleCreateRequest.kt
@@ -3,7 +3,6 @@ package com.kw.api.domain.bundle.dto.request
 import com.kw.data.domain.bundle.Bundle
 import com.kw.data.domain.member.Member
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.Size
 
 data class BundleCreateRequest(
     @field:NotBlank(message = "이름은 필수입니다.")
@@ -11,7 +10,6 @@ data class BundleCreateRequest(
 
     val shareType: Bundle.ShareType,
 
-    @field:Size(max = 3, message = "태그는 최대 3개까지 지정 가능합니다.")
     val tagIds: List<Long>?
 ) {
     fun toEntity(member: Member): Bundle {
