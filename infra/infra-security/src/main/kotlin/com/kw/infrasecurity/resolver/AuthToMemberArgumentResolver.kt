b6d0de316d21510938ca71b27f8079ba91068dfa
KarmaPol <kkhkr98@gmail.com>
2024-03-05 14:57:39 +0900

    fix: argumetn resolver 수정

--- infra/infra-security/src/main/kotlin/com/kw/infrasecurity/resolver/AuthToMemberArgumentResolver.kt
+++ infra/infra-security/src/main/kotlin/com/kw/infrasecurity/resolver/AuthToMemberArgumentResolver.kt
@@ -2,6 +2,7 @@ package com.kw.infrasecurity.resolver
 
 import com.kw.data.domain.member.Member
 import com.kw.data.domain.member.repository.MemberRepository
+import com.kw.infrasecurity.oauth.OAuth2UserDetails
 import org.springframework.core.MethodParameter
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User
@@ -28,8 +29,18 @@ class AuthToMemberArgumentResolver(val memberRepository: MemberRepository) : Han
         binderFactory: WebDataBinderFactory?
     ): Any? {
         val authentication = SecurityContextHolder.getContext().authentication ?: throw IllegalArgumentException("접근이 거부되었습니다.")
-        val userDetails = authentication.principal as DefaultOAuth2User
-        val member = memberRepository.findMemberByEmail(userDetails.getAttribute<String>("email").toString()) ?: throw IllegalArgumentException("존재하지 않는 회원입니다.")
+
+        val userDetails = authentication.principal
+        var email: String = ""
+
+        if(userDetails is DefaultOAuth2User){
+            email = userDetails.getAttribute<String>("email").toString()
+        }
+        else if(userDetails is OAuth2UserDetails) {
+            email = userDetails.email
+        }
+
+        val member = memberRepository.findMemberByEmail(email) ?: throw IllegalArgumentException("존재하지 않는 회원입니다.")
 
         isMemberWithdraw(member)
 
