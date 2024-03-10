65fa2114a5f486966c6f506875f207ae5bcbf823
Hanna Lee <8annahxxl@gmail.com>
2024-03-10 17:45:18 +0900

    fix: env

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:17-jdk
 ARG JAR_PATH=server/api/build/libs
 COPY ${JAR_PATH}/api-0.0.1-SNAPSHOT.jar api.jar
-ENTRYPOINT ["java", "-jar", "-Dspring.profiles.active=$PROFILE"]
+ENTRYPOINT ["java", "-jar", "-Dspring.profiles.active=${SPRING_PROFILES_ACTIVE}"]
 CMD ["/api.jar"]
--- server/api/src/main/kotlin/com/kw/api/ApiApplication.kt
+++ server/api/src/main/kotlin/com/kw/api/ApiApplication.kt
@@ -2,7 +2,6 @@ package com.kw.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication(scanBasePackages = arrayOf("com.kw"))
 class ApiApplication
