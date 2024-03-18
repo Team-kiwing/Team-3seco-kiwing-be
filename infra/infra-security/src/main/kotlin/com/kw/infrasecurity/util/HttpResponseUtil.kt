ed93e136a659bd50b7ac9b7061637fd30b3a02dc
KarmaPol <kkhkr98@gmail.com>
2024-03-19 02:53:42 +0900

    fix: #90 첫 로그인 시 isLoginFirst 불린 값 제공하도록 수정

--- infra/infra-security/src/main/kotlin/com/kw/infrasecurity/util/HttpResponseUtil.kt
+++ infra/infra-security/src/main/kotlin/com/kw/infrasecurity/util/HttpResponseUtil.kt
@@ -17,16 +17,12 @@ class HttpResponseUtil(@Value("\${client-redirect-url}") val REDIRECT_URL: Strin
     private val objectMapper = ObjectMapper()
 
     fun writeResponse(response : HttpServletResponse, accessToken : String, refreshToken : String, isSignUp : Boolean) {
-        var redirectUrl = REDIRECT_URL
-        if(isSignUp) {
-            redirectUrl += "/register"
-        }
-        else {
-            redirectUrl += "/auth"
-        }
+        var redirectUrl = REDIRECT_URL + "/auth"
+
         val sb = StringBuffer(redirectUrl)
         sb.append("?").append("access-token=").append(accessToken)
         sb.append("&").append("refresh-token=").append(refreshToken)
+        sb.append("&").append("isFirstLogin=").append(isSignUp)
 
         response.sendRedirect(sb.toString())
     }
