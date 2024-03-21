e458da4dea9d0369c8d2074b7130f981ac6bad91
KarmaPol <kkhkr98@gmail.com>
2024-03-21 18:49:48 +0900

    chore: 람다 배포용 설정 추가

--- infra/infra-lambda/build.gradle.kts
+++ infra/infra-lambda/build.gradle.kts
@@ -8,6 +8,10 @@ bootJar.enabled = false
 // 외부에서 의존하기 위한 jar로 생성하는 옵션, main이 없는 라이브러리에서는 true로 비활성화함
 jar.enabled = true
 
+plugins {
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
 dependencies {
     implementation(project(":data"))
 
@@ -16,4 +20,27 @@ dependencies {
 
     // lambda
     implementation("org.springframework.cloud:spring-cloud-function-web:4.1.0")
+    implementation("org.springframework.cloud:spring-cloud-function-kotlin:4.1.0")
+    implementation("org.springframework.cloud:spring-cloud-function-adapter-aws:4.1.0")
+    implementation("com.amazonaws:aws-lambda-java-events:3.11.4")
+    implementation("com.amazonaws:aws-lambda-java-core:1.2.3")
+    implementation("org.springframework.boot.experimental:spring-boot-thin-layout:1.0.28.RELEASE")
+}
+
+configurations {
+    create("shaded")
+}
+
+val shade by tasks.registering(Jar::class) {
+    from(configurations["shaded"].map { if (it.isDirectory) it else zipTree(it) })
+    configurations["shaded"].forEach { dep ->
+        from(dep) {
+            exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
+        }
+    }
+    archiveClassifier.set("aws")
+}
+
+artifacts {
+    add("archives", shade)
 }
---
+++ infra/infra-lambda/src/main/kotlin/com/kw/infralamda/handler/LambdaHandler.kt
@@ -0,0 +1,6 @@
+package com.kw.infralamda.handler
+
+import org.springframework.cloud.function.adapter.aws.FunctionInvoker
+
+class LambdaHandler: FunctionInvoker() {
+}
