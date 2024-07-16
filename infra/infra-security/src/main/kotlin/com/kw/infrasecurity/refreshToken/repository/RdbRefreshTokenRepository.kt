682c2c245508ce565b1829b33464f624854f16b9
KarmaPol <kkhkr98@gmail.com>
2024-07-16 23:59:26 +0900

    feat: jpaRefreshToken 시간 체크 기능 추가

--- infra/infra-security/src/main/kotlin/com/kw/infrasecurity/refreshToken/repository/RdbRefreshTokenRepository.kt
+++ infra/infra-security/src/main/kotlin/com/kw/infrasecurity/refreshToken/repository/RdbRefreshTokenRepository.kt
@@ -3,6 +3,8 @@ package com.kw.infrasecurity.refreshToken.repository
 import com.kw.infrasecurity.refreshToken.entity.JpaRefreshToken
 import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Repository
+import java.time.Duration
+import java.time.LocalDateTime
 
 @Primary
 @Repository
@@ -19,9 +21,26 @@ class RdbRefreshTokenRepository(val jpaRefreshTokenRepository: JpaRefreshTokenRe
     }
 
     override fun findByRefreshToken(refreshToken: String): Long? {
-        val jpaRefreshToken = jpaRefreshTokenRepository.findByRefreshToken(refreshToken) ?: throw RuntimeException()
+        val jpaRefreshToken = jpaRefreshTokenRepository.findByRefreshToken(refreshToken) ?: return null
+
+        if(isRefreshTokenExpired(jpaRefreshToken))
+            return null
+
         return jpaRefreshToken.memberId
+    }
+    // TODO 테스트 해보기
+
+    private fun isRefreshTokenExpired(refreshToken: JpaRefreshToken): Boolean {
+        val timeDifference = Duration.between(LocalDateTime.now(), refreshToken.createdAt)
+        if(timeDifference.toSeconds() < REFRESH_TOKEN_EXPIRE_LONG) {
+            return false
+        }
+        return true
     }
 
     // TODO 만료된 refresh token 처리 구현하기
+    companion object {
+        private const val REFRESH_TOKEN_EXPIRE_LONG = 259200L
+    }
+    // TODO 만료기간 yml에서 가져오게 수정하기
 }
