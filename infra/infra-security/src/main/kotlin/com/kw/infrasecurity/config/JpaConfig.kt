13f5158a3c2afd744a7918a197169f495d338c48
KarmaPol <kkhkr98@gmail.com>
2024-07-26 21:12:30 +0900

    fix: 어노테이션 수정

--- infra/infra-security/src/main/kotlin/com/kw/infrasecurity/config/JpaConfig.kt
+++ infra/infra-security/src/main/kotlin/com/kw/infrasecurity/config/JpaConfig.kt
@@ -2,11 +2,9 @@ package com.kw.infrasecurity.config
 
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @Configuration
-@EnableJpaAuditing
 @EntityScan("com.kw.infrasecurity.refreshToken")
 @EnableJpaRepositories(basePackages = ["com.kw.infrasecurity.refreshToken"])
 class JpaConfig {
